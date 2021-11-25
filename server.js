1d9e7f3d72516a914fa772a6e81d46da2d236b4f
draeder <draeder@gmail.com>
2021-11-24 18:39:43 -0600

    add public folder for express

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static('public'))
+
 // sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
