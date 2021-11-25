9977528c10a8f565aa42ef6e495ad0eefbeac0b1
draeder <draeder@gmail.com>
2021-11-25 12:54:54 -0600

    .

--- browser.js
+++ browser.js
@@ -26,7 +26,7 @@ const input = $('#input')
 const inputform = $('#inputform')
 
 console.log('* Connecting to channel', channelName)
-console.log(channel.peers.peer[0].channelName)
+console.log(channel.peers.peer)
 
 let peerCount = 0
 let peers = [{user: userName, id: ''}]
