da8383c61dd162f654531b46e060ca649c9eb717
draeder <draeder@gmail.com>
2021-11-24 19:03:49 -0600

    fixed path?

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-path.join(__dirname, '/public')
+app.use(express.static(__dirname + '/public'))
 
 // sendFile will go here
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log('Server started at http://localhost:' + port)
